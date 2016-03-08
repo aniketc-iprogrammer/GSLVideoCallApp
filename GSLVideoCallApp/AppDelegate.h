b7fc426948b02304d48bb4392712a109ffada10d
Aniket <aniket@Anikets-MacBook-Pro.local>
2016-03-08 13:02:37 +0530

    Created an empty project

---
+++ GSLVideoCallApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GSLVideoCallApp
+//
+//  Created by Aniket on 08/03/16.
+//  Copyright (c) 2016 GSLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
